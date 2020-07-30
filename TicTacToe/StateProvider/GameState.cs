1f83bb23c5ac09ddb057005fccadce9e38115142
Kevin Santacruz <defonix0@gmail.com>
2020-07-29 23:13:57 -0500

    control click for each square

--- TicTacToe/StateProvider/GameState.cs
+++ TicTacToe/StateProvider/GameState.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TicTacToe.StateProvider
 {
     public class GameState
     {
         public int turn { get; set; }
+        public string[] squares = new string[9];
         public string symbol { get; set; }
         public event Action OnChange;
         public GameState()
