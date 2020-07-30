723ff839a91d0c7c84e9f9bb670c48751a4100ef
Kevin Santacruz <defonix0@gmail.com>
2020-07-29 22:50:45 -0500

    state manage to control each turn

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TicTacToe.StateProvider;
 
 namespace TicTacToe
 {
@@ -16,7 +17,7 @@ namespace TicTacToe
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
+            builder.Services.AddSingleton<GameState>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
---
+++ TicTacToe/StateProvider/GameState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.StateProvider
+{
+    public class GameState
+    {
+        public int turn { get; set; }
+        public string symbol { get; set; }
+        public event Action OnChange;
+        public GameState()
+        {
+            symbol = "X";
+            turn = 1;
+        }
+        public void nextTurn()
+        {
+            turn++;
+            symbol = (turn % 2 == 0 ? "O" : "X");
+            NotifyStateChanged();
+        }
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
